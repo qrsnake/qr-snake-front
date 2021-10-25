19454d30ecbf02cc018adfadccda150e73337353
albertobazilio98 <albertobazilio98@gmail.com>
2021-10-24 23:55:38 -0400

    [NEW] pwa!

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import router from './router';
+import './registerServiceWorker'
 
 Vue.config.productionTip = false;
 
