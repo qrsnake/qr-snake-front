28bae58b1f6e818d11741b0e42b6bbdd1e88c491
Thayane Batista <thayane@jeraapps.com>
2021-11-16 19:07:28 -0400

    [ADD] array species

--- src/views/ListaEspecies.vue
+++ src/views/ListaEspecies.vue
@@ -1,10 +1,14 @@
 <template>
 <div>
-  
-<v-card>
-
-
-  <v-btn
+  <v-card>
+    <v-btn
+      :v-for="especies in species"
+      v-bind:key="especies"
+    > 
+      {{ especies }}
+    </v-btn>
+  </v-card>
+  <!-- <v-btn
     elevation="10"
     shaped
     class="ma-2"
@@ -13,11 +17,32 @@
     @click="animal"
 
   >
-    animal 🍕
-  </v-btn>
-  
-
-  </v-card>
+    Cobrinha
+  </v-btn> -->
 
 </div>
 </template>
+
+<script>
+
+export default {
+  props: {
+    species: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      especies: [
+        'Jararaca','coral', 'anaconda',
+      ]
+    };
+  },
+  // computed: {
+  //   todasEspecies() {
+  //     return this.especies;
+  //   }
+  // }
+};
+</script>
