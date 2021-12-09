e0fc745381d329c9802cce91fdd7906352680202
Thayane Batista <thayane@jeraapps.com>
2021-12-09 14:30:15 -0400

    [fix] css btn

---
+++ src/views/InfoEspecie.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <div class="camp-css">
+      <v-text-field
+        label="Nome cientifico da especie"
+        outlined
+        align="center"
+        disabled
+      ></v-text-field>
+
+      <v-text-field
+        label="Nome comum da especie"
+        outlined
+        align="center"
+        disabled
+      ></v-text-field>
+
+      <v-text-field
+        label="Reprodução"
+        outlined
+        align="center"
+        disabled
+      ></v-text-field>
+
+      <v-text-field
+        label="Habitat"
+        outlined
+        align="center"
+        disabled
+      ></v-text-field>
+
+      <v-text-field
+        label="Dentição"
+        outlined
+        align="center"
+        disabled
+      ></v-text-field>
+    </div>
+    <div class="main-css">
+      <v-btn
+        class="ma-2"
+        outlined
+        color="indigo"
+      >
+        Editar
+    </v-btn>
+
+    <v-btn
+        class="ma-2"
+        outlined
+        color="indigo"
+      >
+        Deletar
+    </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InfoEspecie",
+  data: () => ({
+    nomeCientifico: '',
+    nomeComum: '',
+    reproducao: '',
+    habitat: '',
+    denticao: '',
+    expandInfos: false,
+  }),
+  methods: {
+    show() {
+      this.expandInfos = !this.expandInfos;
+    },
+  },
+};
+</script>
+
+<style>
+.camp-css{
+  padding: 1% 3%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+
+</style>
--- src/views/ListaEspecies.vue
+++ src/views/ListaEspecies.vue
@@ -10,41 +10,50 @@
         Voltar
       </v-btn>
     </div>
-    <v-card 
-      class="mx-auto" 
-    >
-      <v-card-title
-        @click="show =! show"
-      > 
-        Especie 
-        <v-icon
-          class="icon-button"
-        >{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
-      </v-card-title>
-      <v-expand-transition>
-        <div v-show="show">
-          <v-divider></v-divider>
-          <v-card-text>
-              INFOS DAS ESPECIES
-          </v-card-text>
-        </div>
-      </v-expand-transition>
-    </v-card>
+          <!-- v-for="especie in especies"
+          :key="especie.tipo" -->
+      <v-flex
+      >
+        <v-card 
+          class="mx-auto" 
+        >
+          <v-card-title
+            @click="show =! show"
+          > 
+            Especie
+            <v-icon
+              class="icon-button"
+            >{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
+          </v-card-title>
+          <v-expand-transition>
+            <div v-show="show">
+              <v-divider></v-divider>
+              <v-card-text>
+                  <info-especie ref="infos"></info-especie>
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
+      </v-flex>
   </div>
 </template>
 
 <script>
+import InfoEspecie from './InfoEspecie.vue';
 //import animalResource from '../api/resources/animal';
 export default {
   data: () => ({
     show: false,
-    nomeCientifico:'', 
-    nomeComum: '',
-    reproducao: '',
-    tipoDeAlimentação: '',
-    habitat: '',
-    denticao: '', 
+    especies: [
+      { tipo: ' Especie 1 '},
+      { tipo: ' Especie 2 '},
+      { tipo: ' Especie 3 '},
+      { tipo: ' Especie 4 '},
+    ]
   }),
+  components: {
+    InfoEspecie,
+  },
   methods: {
     goBack() {
       this.$router.push("/search");
@@ -52,7 +61,10 @@ export default {
   },
   async get() {
     //const especies = await animalResource.get({}); //precisa de um get para especies
-  }
+  },
+  async abrirInfoEspecie() {
+    this.$refs.infos.show();
+  },
 };
 </script>
 
